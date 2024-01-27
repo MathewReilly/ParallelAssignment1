b1f762919572bd551dc24c68627e7cd92e2f45f9
Mathew-Reilly <mathewreilly2735@outlook.com>
2024-01-26 21:00:50 -0500

    updated code and added comments

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -1,4 +1,16 @@
+// Mathew Reilly
+// COP4520
+// Assignment 1
+// Description: This is a program that finds all of the primes from 1 to 10^8 using
+//              multithreading.
+//
+//              The output is printed to a "primes.txt" file where the first
+//              value is the time in seconds the threads took to complete,
+//              the second value is the number of primes found, and the third
+//              value is the sum of all of the primes. The following line is
+//              the top ten primes listed lowest to highest.
 #include <iostream>
+#include <fstream>
 #include <thread>
 #include <mutex>
 #include <cmath>
@@ -10,12 +22,14 @@ using namespace std;
 
 mutex counterMutex;
 
-// How large of n are we using?
+// Where we input the size we would like to check for primes
+// this vector will mark all of the composite values, leaving only the primes
+// unmarked.
 vector<bool> nVector(100000000);
 int64_t sizeOfN;
 int64_t sqrtOfN;
 
-//ticket counter implementation from Lecture 1
+//ticket counter implementation related to Lecture 1 slides
 class Counter
 {
 private: int64_t value;
@@ -35,20 +49,9 @@ public:
 
     return temp;
   }
-
-  int64_t get()
-  {
-    return value;
-  }
-
-  // must not be performed with threading, okay to use before
-  void increment()
-  {
-    value++;
-  }
 };
 
-//should these variables be in global space?!?!?
+// Global counter distibuting tasks to threads.
 Counter compositeCounter(1);
 
 bool isPrime(int64_t potentialPrime)
@@ -56,11 +59,9 @@ bool isPrime(int64_t potentialPrime)
   // We need to factorize our potentialPrime value
   // we will set our initial condition by finding the square root of our value
   // this allows us to limit the total potential factors to check by skipping duplicates
-  // although cieling is more accurate it accrues more work???? - needs to be tested with cieling
   int64_t totalFactorsToCheck = static_cast<int64_t>(sqrt(potentialPrime)) + 1;
 
-  //int64_t totalFactorsToCheck = potentialPrime / 2;
-
+  // Trivial cases done on their own
   if(potentialPrime == 2)
   {
     return true;
@@ -69,34 +70,36 @@ bool isPrime(int64_t potentialPrime)
     return false;
   }
 
+  // look for factors, if one found return false, otherwise it is prime
   for(int i = 2; i <= totalFactorsToCheck; i++)
   {
     if(potentialPrime % i == 0)
     {
-      return false; // we found a factor
+      return false;
     }
   }
   return true;
 }
 
+//This lock is used to protect the shared global vector tracking composites.
 mutex vectorMutex;
-
 
 void markComposites()
 {
 
-  //set<int64_t> threadComposites;
+  // will grab the threads initial value to check and or mark composites from
+  int64_t j = compositeCounter.getAndIncrement();
 
-  // cout << " threads begin " << endl;
-  // this could grab something one below and then get to later code and go above limit...
-  int64_t j = compositeCounter.getAndIncrement(); // i think this can still be wrong with mutual exclusion...
   // This will be where we mark composites
-  // cout << " gets j " << endl;
   while(j <= sqrtOfN)
   {
 
+    // a temporary vector to make all of the composites of an initial prime.
+    // used to divide the work of updating the composite vector with lock
     vector<int64_t> curComposites;
 
+    // if we find a prime value in the range of 1 to (sqrt(N) + 1), mark all of
+    // the composites of it, otherwise, mark that value as a non-prime value.
     if(isPrime(j))
     {
       // mark all composites
@@ -110,6 +113,8 @@ void markComposites()
       curComposites.push_back(j);
     }
 
+    // once we've gathered all of the composites related to a prime, or marked an
+    // single composite value, update our main list of composites with this information
     vectorMutex.lock();
     for(int64_t indexComposites = 0; indexComposites < curComposites.size(); indexComposites++)
     {
@@ -117,6 +122,7 @@ void markComposites()
     }
     vectorMutex.unlock();
 
+    // grab our next value to check before continuing.
     j = compositeCounter.getAndIncrement();
   }
 
@@ -125,12 +131,12 @@ void markComposites()
 int64_t totalPrimes = 0;
 int64_t sumPrimes = 0;
 
-// this is a helper function setting values so threads to not need to repeat this work
+// this is a helper function setting values relating to the vector's dimensions
 void prepForFindingPrimes()
 {
   // get the size of your n values to check for primes
   sizeOfN = nVector.size();
-  cout << sizeOfN << endl;
+  //cout << sizeOfN << endl;
   // get sqrt(n) + 1 of primes to use for Eratosthenes
   sqrtOfN = sqrt(sizeOfN) + 1;
 }
@@ -138,7 +144,6 @@ void prepForFindingPrimes()
 
 int main()
 {
-
   // This prepares the necessary data for performing Eratosthenes because
   // they are global.
   prepForFindingPrimes();
@@ -146,6 +151,7 @@ int main()
   vector<int64_t> largestPrimes;
   int primeCounter = 10;
 
+  // start the clock and run the threads to completion
   auto start = chrono::high_resolution_clock::now();
 
   thread t1(markComposites);
@@ -169,6 +175,11 @@ int main()
   auto stop = chrono::high_resolution_clock::now();
   auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
 
+  // calculate time in seconds
+  double timeSeconds = duration.count() / 1000000.0;
+
+  // make a list of our ten largest primes, count the number of primes, and the sum
+  // of primes by iterating through the vecor of marked composites
   for(int64_t getPrimeData = sizeOfN - 1; getPrimeData > 0; getPrimeData--)
   {
     if(nVector[getPrimeData] == 0)
@@ -185,12 +196,19 @@ int main()
     }
   }
 
-  cout << duration.count() << " " << totalPrimes << " " << sumPrimes << endl;
+  // Create the output file primes.txt
+  ofstream primesPrintFile("primes.txt");
 
+  // print the time in seconds, the number of primes found, and the sum of the primes
+  primesPrintFile << timeSeconds << " " << totalPrimes << " " << sumPrimes << endl;
+
+  // print the 10 largest from smallest to largest
   for(int lp = largestPrimes.size() - 1; lp >= 0; lp--)
   {
-    cout << largestPrimes[lp] << " ";
+    primesPrintFile << largestPrimes[lp] << " ";
   }
+
+  primesPrintFile.close();
 
   return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ParallelAssignment1
-Repository for assignment 1, finding all primes within n.
+This program will find all of the primes from 1 to 10^8 using 8 threads.
+
+To run with gcc:
+  gcc -lstdc++ -std=c++11 -o primes.out Assignment1.cpp
+
+  ./primes.out
+
+To run with g++:
+  g++ -std=c++11 -o primes.out Assignment1.cpp
+
+  ./primes.out
